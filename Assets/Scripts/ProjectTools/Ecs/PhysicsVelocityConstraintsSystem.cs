8962611d83b994a6bcadad3b14f27e1893ed3836
EdwardBrave <edward.brave45@gmail.com>
2025-08-08 16:23:07 +0300

    added PhysicsConstraints system (rigidbody extension)

---
+++ Assets/Scripts/ProjectTools/Ecs/PhysicsVelocityConstraintsSystem.cs
@@ -0,0 +1,43 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Physics;
+using Unity.Physics.Systems;
+
+namespace ProjectTools.Ecs
+{
+    [UpdateInGroup(typeof(PhysicsSystemGroup), OrderLast = true)]
+    [BurstCompile]
+    public partial struct PhysicsVelocityConstraintsSystem : ISystem
+    {
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<PhysicsConstraints>();
+            state.RequireForUpdate<PhysicsVelocity>();
+        }
+        
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var positionJobHandle = new PositionConstraintsJob().ScheduleParallel(state.Dependency);
+            state.Dependency = positionJobHandle;
+        }
+    }
+    
+    [BurstCompile]
+    public partial struct PositionConstraintsJob : IJobEntity
+    {
+        [BurstCompile]
+        public void Execute(ref PhysicsVelocity velocity, in PhysicsConstraints constraints)
+        {
+            var freezePosition = constraints.freezePosition;
+            if (freezePosition.x) velocity.Linear.x = 0;
+            if (freezePosition.y) velocity.Linear.y = 0;
+            if (freezePosition.z) velocity.Linear.z = 0;
+            
+            var freezeRotation = constraints.freezeRotation;
+            if (freezeRotation.x) velocity.Angular.x = 0;
+            if (freezeRotation.y) velocity.Angular.y = 0;
+            if (freezeRotation.z) velocity.Angular.z = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/ProjectTools/Ecs/RigidbodyConstraintsAuthoring.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace ProjectTools.Ecs
+{
+    public struct PhysicsConstraints : IComponentData
+    {
+        public bool3 freezePosition;
+        public bool3 freezeRotation;
+    }
+    
+    public class RigidbodyBaker : Baker<Rigidbody>
+    {
+        public override void Bake(Rigidbody authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            var constraints = authoring.constraints;
+            AddComponent(entity, new PhysicsConstraints
+            {
+                freezePosition = constraints.HasFlag(RigidbodyConstraints.FreezePosition) ? new bool3(true, true, true) :
+                    new bool3(
+                        constraints.HasFlag(RigidbodyConstraints.FreezePositionX),
+                        constraints.HasFlag(RigidbodyConstraints.FreezePositionY),
+                        constraints.HasFlag(RigidbodyConstraints.FreezePositionZ)
+                        ),
+                freezeRotation = constraints.HasFlag(RigidbodyConstraints.FreezeRotation) ? new bool3(true, true, true) :
+                    new bool3(
+                        constraints.HasFlag(RigidbodyConstraints.FreezeRotationX),
+                        constraints.HasFlag(RigidbodyConstraints.FreezeRotationY),
+                        constraints.HasFlag(RigidbodyConstraints.FreezeRotationZ)
+                        ),
+            });
+        }
+    }
+}
